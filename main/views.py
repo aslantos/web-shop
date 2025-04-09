cbdaa1469153befea704e0755ec870eea9eaf167
aslantos <aslantb007@gmail.com>
2025-04-09 16:32:37 +0500

    add: load static, наследование шаблонов, extends, base шаблон

--- main/views.py
+++ main/views.py
@@ -2,9 +2,18 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
-    
+    context = {
+        'title': 'Home - Главная',
+        'content': "Магазин мебели HOME"
+    }
 
-    return render(request, 'main/index.html')
+    return render(request, 'main/index.html', context)
 
 def about(request):
-    return HttpResponse('About page')
+    context = {
+        'title': 'Home - О нас',
+        'content': "О нас",
+        'text_on_page': "Мы магазин мебели скорость качество кайф."
+    }
+
+    return render(request, 'main/about.html', context)
