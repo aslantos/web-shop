9cdeadb56a5f41a29ca223cbd3c4e748a86637ff
Klins74 <aleksandrutepov75@gmail.com>
2025-04-17 00:35:18 +0500

    Update jquery-events.js

--- static/deps/js/jquery-events.js
+++ static/deps/js/jquery-events.js
@@ -4,24 +4,42 @@ $(document).ready(function () {
 
     // Получаем элемент оповещения по ID
     const notification = $('#notification');
-    // Убираем оповещение через заданное время
+    // Убираем оповещение через заданное время с плавным исчезновением
     if (notification.length > 0) {
         setTimeout(() => {
-            notification.alert('close');
+            notification.fadeOut(500, () => {
+                notification.alert('close');
+            });
         }, NOTIFICATION_TIMEOUT);
     }
 
     // Обработчик клика по значку корзины для открытия модального окна
-    $('#modalButton').on('click', () => {
-        const modal = $('#exampleModal');
-        modal.appendTo('body'); // Переносим модальное окно в конец body
-        modal.modal('show'); // Показываем модальное окно
-    });
+    const modalButton = $('#modalButton');
+    if (modalButton.length > 0) {
+        modalButton.on('click', () => {
+            const modal = $('#exampleModal');
+            if (modal.length > 0) {
+                modal.appendTo('body');
+                modal.fadeIn(300); // Плавное появление
+                modal.modal('show');
+            } else {
+                console.error('Modal element not found.');
+            }
+        });
+    } else {
+        console.error('Modal button not found.');
+    }
 
     // Обработчик клика по кнопке закрытия модального окна
-    $('#exampleModal .btn-close').on('click', () => {
-        $('#exampleModal').modal('hide'); // Закрываем модальное окно
-    });
+    const closeModalButton = $('#exampleModal .btn-close');
+    if (closeModalButton.length > 0) {
+        closeModalButton.on('click', () => {
+            const modal = $('#exampleModal');
+            modal.fadeOut(300, () => { // Плавное исчезновение
+                modal.modal('hide');
+            });
+        });
+    }
 
     // Обработчик изменения состояния радиокнопки выбора способа доставки
     $("input[name='requires_delivery']").on('change', function () {
@@ -29,9 +47,19 @@ $(document).ready(function () {
         const deliveryField = $("#deliveryAddressField"); // Поле ввода адреса доставки
 
         if (selectedValue === "1") {
-            deliveryField.addClass('visible'); // Добавляем CSS-класс для отображения
+            deliveryField.slideDown(300).addClass('visible'); // Плавное появление
         } else {
-            deliveryField.removeClass('visible'); // Убираем CSS-класс для скрытия
+            deliveryField.slideUp(300, () => {
+                deliveryField.removeClass('visible'); // Плавное исчезновение
+            });
         }
     });
+
+    // Переключение темной темы
+    const themeToggle = $('#themeToggle');
+    if (themeToggle.length > 0) {
+        themeToggle.on('click', () => {
+            $('body').toggleClass('dark-theme');
+        });
+    }
 });
